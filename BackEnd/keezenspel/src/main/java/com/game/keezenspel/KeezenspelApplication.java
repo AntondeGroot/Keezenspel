7560db264434600033891d1d2b8cd52342804698
Anton de Groot <aammdegroot@gmail.com>
2024-01-05 20:58:04 +0100

    003 fix folder structure

---
+++ BackEnd/keezenspel/src/main/java/com/game/keezenspel/KeezenspelApplication.java
@@ -0,0 +1,13 @@
+package com.game.keezenspel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeezenspelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KeezenspelApplication.class, args);
+	}
+
+}
