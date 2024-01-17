532182a097d0433ae9be727ae8a3048d38fd54dc
Anton de Groot <aammdegroot@gmail.com>
2024-01-17 16:07:11 +0100

    009

--- BackEnd/keezenspel/src/main/java/com/game/keezenspel/WebSocketServer/WebSocketConfig.java
+++ BackEnd/keezenspel/src/main/java/com/game/keezenspel/WebSocketServer/WebSocketConfig.java
@@ -1,4 +1,4 @@
-package com.game.keezenspel;
+package com.game.keezenspel.WebSocketServer;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
