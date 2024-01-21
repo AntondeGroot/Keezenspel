029089bd18875b09fda5bcb43fd7e10faea05593
Anton de Groot <aammdegroot@gmail.com>
2024-01-21 15:59:44 +0100

    016 fix websocket client

--- BackEnd/keezenspel/src/main/java/com/game/keezenspel/WebSocketServer/WebSocketConfig.java
+++ BackEnd/keezenspel/src/main/java/com/game/keezenspel/WebSocketServer/WebSocketConfig.java
@@ -9,16 +9,14 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry config) {
+        config.enableSimpleBroker("/topic");
+        config.setApplicationDestinationPrefixes("/app");
+    }
 
-  @Override
-  public void configureMessageBroker(MessageBrokerRegistry config) {
-    config.enableSimpleBroker("/topic");
-    config.setApplicationDestinationPrefixes("/app");
-  }
-
-  @Override
-  public void registerStompEndpoints(StompEndpointRegistry registry) {
-    registry.addEndpoint("/gs-guide-websocket");
-  }
-
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/ws").setAllowedOrigins("http://localhost:4200").withSockJS();
+    }
 }
